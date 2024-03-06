86056e8bffc867afa98bdcd53482619ae4556653
wisnubaik <wisnuap2024@gmail.com>
2024-03-06 09:54:20 +0700

    htmlForm

---
+++ html lanjutan/action_page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Kirim Data</title>
+</head>
+<body>
+    <form action="/action_page.php" method="post">
+        <label for="nama">Nama:</label> <br>
+        <input type="text" name="nama" id="nama" required> <br>
+        <label for="nama">Nama Belakang:</label> <br>
+        <input type="text" name="nama" id="nama" required> <br>
+        <input type="submit" value="Kirim">
+    </form>
+</body>
+</html>
